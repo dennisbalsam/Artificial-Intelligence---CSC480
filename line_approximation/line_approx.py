aa78edeff1596f16181bec416605ed8d4e2add63
Dennis Krupitsky <d.krupitsky@gmail.com>
2020-02-03 15:41:32 -0500

    very simple program, we are trying to approximate a line using random points

--- line_approximation/line_approx.py
+++ line_approximation/line_approx.py
@@ -4,17 +4,7 @@ import random
 import numpy
 
 
-m=s=0
-
-L=10
-
-
-for j in range(2,10):
-    L=L*10
-    s=0
-    for i in range(1,L):
-        r=random.uniform(-.1,.1)
-        s=s+r
-        m=1.0*s/(L*1.0)
-
-print("j=",j,"s=",s,"L=",L,"m=",m)
+for x in range(0,10):
+    r=random.uniform(-.1,.1)
+    y = x + (1+r)
+    print("Iteration: ", x, " | ", "Equation: " , "y=", y)
