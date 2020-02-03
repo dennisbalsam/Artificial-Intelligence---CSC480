4d2ec8e66001ed05f9e28b14d07fbd097a34619a
Dennis Krupitsky <d.krupitsky@gmail.com>
2020-02-03 00:30:26 -0500

    law of large numbers commit

--- largest-number/largeNumber.py
+++ largest-number/largeNumber.py
@@ -1,23 +1,30 @@
 import sys
 import math
 import random
-from tqdm import tqdm
 
-#import numpy
-
-
-#a=numpy.arange(100).reshape(1)
-m=s=0
-L=10
+# program to represent the law of large numbers, which uses an claims that as the number of trials increases, 
+# the actual probability of the trial approaches the expected probability
+# i utilize a common trial of flipping a coin many times which should reach a probability of 0.5 or 50%. I will use different number of attempts for this
 
 
-for j in tqdm(range(2,10), desc="outerloop"):
-    L=L*10
-    s=0
-    for i in tqdm(range(1,L),desc="innerloop"):
-        r=random.uniform(-.1,.1)
-        s=s+r
-        m=1.0*s/(L*1.0)
+# some variables
+headspercentage = 0.0
+tailspercentage = 0.0
+value = 1
 
+for iteration in range(0,7):
+    value*=10
+    # reset values
+    heads = 0
+    tails = 0
+    for i in range(0,value):
+        #get random number emulating a coin flip
+        flip= random.randint(0,1)
+        if flip == 0:
+            heads +=1
+        else:
+            tails+=1
+    headspercentage = (heads / value) * 100
+    tailspercentage = (tails / value) * 100
+    print("Iterations: ", value, " | Heads: ",heads,"|  Tails: ",tails," |  Heads-Percentage=",round(headspercentage, 3), " | Tails-Percentage=",round(tailspercentage, 3)) 
 
-print("j=",j,"s=",s,"L=",L,"m=",m)
