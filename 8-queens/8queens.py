b68fdbd9af8fdd2e72b68b52238d4fa52541063d
Dennis Krupitsky <d.krupitsky@gmail.com>
2020-03-07 14:38:30 -0500

    new file'

--- 8-queens/8queens.py
+++ 8-queens/8queens.py
@@ -1,24 +1,107 @@
-import random
-import sys
+board_size = 8
+board = [[0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0]]
 
+# This takes the current board and a col, and returns true if a queen is placed
+def place_queen(board, col):
+    
+    # Bool val. keeps track if a queen has been placed or not
+    queen_placed = False
 
-# eight queens problem is the problem of placing eight queens on an 8×8 chessboard such that 
-# none of them attack one another 
+    for row in range(board_size):
+        # If there is an empty space and it is not threatened by a queen, in that location
+        if(board[row][col] == 0 and validate_placement(board, row, col)):
+            board[row][col] = 1
+            queen_placed = True
+            break
+    
+    return queen_placed
 
-#functions
+# This ensures that the placement of a queen is not threatened by another queen
+def validate_placement(board, row, col):
+    
+    #Checking horizontal and vertical
+    #--------REVIEW -may be screwy
+    for x in range(board_size):
+        #row
+        if board[row][x] == 1:
+            return False
+        #col
+        if board[x][col] == 1:
+            return False
+        
+    #check diag top left
+    i = row - 1
+    j = col - 1
+    while i >= 0 and j >= 0:
+        if board[i][j] == 1:
+            return False
+        i -= 1
+        j -= 1
 
+    #check diag down right
+    i = row + 1
+    j = col + 1
+    while i <= 7 and j <= 7:
+        if board[i][j] == 1:
+            return False
+        i += 1
+        j += 1
+    
+    #check diag top right
+    i = row - 1
+    j = col + 1
+    while i >= 0 and j <= 7:
+        if board[i][j] == 1:
+            return False
+        i -= 1
+        j += 1
+    
+    #check diag down left
+    i = row + 1
+    j = col - 1
+    while i <= 7 and j >= 0:
+        if board[i][j] == 1:
+            return False
+        i += 1
+        j -= 1
+    
+    return True
 
-# create tree for traversel 
-class Tree:
-    def init(self):
-        self.right = None
-        self.left=None
-        self.data=None
+def print_board(board):
+    for x in board:
+        print(x)
 
-root= Tree()
-root.data = "initiate tree"
+# This is what happens when we ask to place 8 queens. [CHECK IMAGE] Only 5 are placed.
+# Currently, the program only allows for direct placements and will stop if there are 
+# no more open spaces. What we want to happen is [CHECK IMAGE2]
+#---------------------------------------------
+#
+# if queen can be placed:
+#     place queen in first available row position     
+# else (if queen can't be placed):
+#     remove current queen
+#     move prev. queen further in its row
+# 
+#---------------------------------------------
+# This is done recursively until we can't continue placing queens. Just having
+# trouble figuring out the recursion 
 
-print(root.data)
+#EXAMPLE
+place_queen(board, 0)
+place_queen(board, 1)
+place_queen(board, 2)
+place_queen(board, 3)
+place_queen(board, 4)
+#NO MORE AVAILABLE SPOTS AFTER THIS POINT
+place_queen(board, 5)
+place_queen(board, 6)
+place_queen(board, 7)
 
-
-# driver/entry point
+print_board(board)
