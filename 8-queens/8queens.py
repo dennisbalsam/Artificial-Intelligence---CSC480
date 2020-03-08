e8e53ed035dcdc521ddc8c8c406de2d3b300c76c
Dennis Krupitsky <d.krupitsky@gmail.com>
2020-03-08 00:18:06 -0500

    getting solutions theyre just not printing

--- 8-queens/8queens.py
+++ 8-queens/8queens.py
@@ -1,40 +1,53 @@
-board_size = 8
-board = [[0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0]]
+''' 8 queens problem is a famous problem involving the placing of 8 queens on a 8x8 chessboard
+such that none of the 8 queens are able to atack each other. There are a total of 92 solutions to this problem '''
 
-# This takes the current board and a col, and returns true if a queen is placed
+# use time library to track how long finding all solutions takes
+import time
+
+# list for all solutions, so we dont waste time printing untill all solutions are found
+total_solutions = []
+# board size
+board_size = 8
+
+# This takes the current board and a col, and returns true if a queen is able to be placed
 def place_queen(board, col):
     
-    # Bool val. keeps track if a queen has been placed or not
-    queen_placed = False
+    global total_solutions
 
+    # Bool val. keeps track if a queen has been placed or not
+    queens_placed = False
+
+    # column is 8 so this is a valid solution
+    if col == 8:
+        # add solution
+        total_solutions.append(board)
+        return True
+
+    # go through every row in the column untila safe place is found
     for row in range(board_size):
         # If there is an empty space and it is not threatened by a queen, in that location
         if(board[row][col] == 0 and validate_placement(board, row, col)):
+            # if validate_placement returns true we place a queen in that row
             board[row][col] = 1
-            queen_placed = True
-            break
+            # use recursion to keep traversing the chess board and placing queens, this will lead to next column, if the recursion continues until the column is 8, then we have found a solution
+            queens_placed = place_queen(board,col +1) or queens_placed
+            # if the recusrion fails to place all 8 queens, we will escape the previous lines calls, and reset the current marked spot to empty and try the next rows until valid solution is found
+            board[row][col] = 0  # essentially this allows for backtracking
+
     
-    return queen_placed
+    # will return false if no valid solution is found, otherwise it will return true and a solution is found
+    return queens_placed
 
 # This ensures that the placement of a queen is not threatened by another queen
 def validate_placement(board, row, col):
-    
-    #Checking horizontal and vertical
-    #--------REVIEW -may be screwy
-    for x in range(board_size):
+    #Checking horizontal row 
+    for x in range(col):
         #row
         if board[row][x] == 1:
             return False
-        #col
-        if board[x][col] == 1:
-            return False
+        #col -------------- column also doesnt have to be checked, we use 1 column per queen anyways
+        # if board[x][col] == 1:
+        #     return False
         
     #check diag top left
     i = row - 1
@@ -45,23 +58,7 @@ def validate_placement(board, row, col):
         i -= 1
         j -= 1
 
-    #check diag down right
-    i = row + 1
-    j = col + 1
-    while i <= 7 and j <= 7:
-        if board[i][j] == 1:
-            return False
-        i += 1
-        j += 1
-    
-    #check diag top right
-    i = row - 1
-    j = col + 1
-    while i >= 0 and j <= 7:
-        if board[i][j] == 1:
-            return False
-        i -= 1
-        j += 1
+
     
     #check diag down left
     i = row + 1
@@ -72,36 +69,59 @@ def validate_placement(board, row, col):
         i += 1
         j -= 1
     
+    ''' We dont have to check the right side since were placing starting form the left, so attacking queens will only be there 
+    # #check diag down right
+    # i = row + 1
+    # j = col + 1
+    # while i <= 7 and j <= 7:
+    #     if board[i][j] == 1:
+    #         return False
+    #     i += 1
+    #     j += 1
+    
+    # #check diag top right
+    # i = row - 1
+    # j = col + 1
+    # while i >= 0 and j <= 7:
+    #     if board[i][j] == 1:
+    #         return False
+    #     i -= 1
+    #     j += 1
+    '''
     return True
 
+# function to print out the board
 def print_board(board):
     for x in board:
         print(x)
+    print()
 
-# This is what happens when we ask to place 8 queens. [CHECK IMAGE] Only 5 are placed.
-# Currently, the program only allows for direct placements and will stop if there are 
-# no more open spaces. What we want to happen is [CHECK IMAGE2]
-#---------------------------------------------
-#
-# if queen can be placed:
-#     place queen in first available row position     
-# else (if queen can't be placed):
-#     remove current queen
-#     move prev. queen further in its row
-# 
-#---------------------------------------------
-# This is done recursively until we can't continue placing queens. Just having
-# trouble figuring out the recursion 
+# -------------------------------------- driver ----------------------------------------------
+# create the board using simple for loop
+board= []
+for j in range(8):
+    row = []
+    for i in range(8):
+        row.append(0)
+    board.append(row)
 
-#EXAMPLE
-place_queen(board, 0)
-place_queen(board, 1)
-place_queen(board, 2)
-place_queen(board, 3)
-place_queen(board, 4)
-#NO MORE AVAILABLE SPOTS AFTER THIS POINT
-place_queen(board, 5)
-place_queen(board, 6)
-place_queen(board, 7)
+#mark the starting time
+timeStart = time.time()
+totalTime = 0
+# find the solutions
+foundSolutions = place_queen(board,0)
+# mark ending time before printing
+totalTime = time.time() - timeStart
 
-print_board(board)
+if foundSolutions:
+    # if solutions are found print them
+    for board in total_solutions:
+        print_board(board)
+else:
+    ("print there are no solutions")
+
+# output the total time it took to find all 92 solutions
+print("The number of solutions is: ", len(total_solutions))
+print("The total time used to find all solutions is: ", totalTime)
+
+
